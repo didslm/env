16d814ab10ab4914f3921537a5744b61503db8f1
Diar <diarselimi92+github@gmail.com>
2023-01-24 11:15:58 +0100

    Add support for structs inside structs

--- env.go
+++ env.go
@@ -11,24 +11,35 @@ func PopulateWithEnv(s interface{}) {
 	for i := 0; i < val.NumField(); i++ {
 		valueField := val.Field(i)
 		typeField := val.Type().Field(i)
+
+		obj := typeField.Tag.Get("obj")
+		if obj != "" && valueField.Kind() == reflect.Struct {
+			PopulateWithEnv(valueField.Addr().Interface())
+			continue
+		}
+
 		tag := typeField.Tag.Get("env")
 		if tag != "" {
 			envValue := os.Getenv(tag)
 			if envValue != "" {
-				switch valueField.Kind() {
-				case reflect.String:
-					valueField.SetString(envValue)
-				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-					intValue, _ := strconv.ParseInt(envValue, 10, 64)
-					valueField.SetInt(intValue)
-				case reflect.Float32, reflect.Float64:
-					floatValue, _ := strconv.ParseFloat(envValue, 64)
-					valueField.SetFloat(floatValue)
-				case reflect.Bool:
-					boolValue, _ := strconv.ParseBool(envValue)
-					valueField.SetBool(boolValue)
-				}
+				assignValue(&valueField, envValue)
 			}
 		}
+	}
+}
+
+func assignValue(field *reflect.Value, value string) {
+	switch field.Kind() {
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		intValue, _ := strconv.ParseInt(value, 10, 64)
+		field.SetInt(intValue)
+	case reflect.Float32, reflect.Float64:
+		floatValue, _ := strconv.ParseFloat(value, 64)
+		field.SetFloat(floatValue)
+	case reflect.Bool:
+		boolValue, _ := strconv.ParseBool(value)
+		field.SetBool(boolValue)
 	}
 }
